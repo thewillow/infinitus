21342e4b294d66e71d35157cd524d60e03a999f6
Willow <thewillowness@pm.me>
2024-05-19 20:53:08 -0700

    fix footer

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Infinitus Ministries'
+copyright = '2024, Willow'
+author = 'Willow'
 
 release = '0.1'
 version = '0.1.0'
